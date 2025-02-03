97492a819318d590aeed9f2c98ce49de78940181
Denis <denis.medel@holon.com.ar>
2025-02-03 16:13:13 -0300

    Curso DevOps Terraform + Github Actions

--- terraform/ec2-script.sh
+++ terraform/ec2-script.sh
@@ -7,7 +7,15 @@ curl -fsSL https://get.docker.com | sh
 
 sudo usermod -aG docker ${USER}
 
+sudo systemctl enable docker
+sudo systemctl start docker
+
+mkdir -p /opt/dockerfiles/nginx
+touch /opt/dockerfiles/nginx/index.html
+echo "Hola Curso" | sudo tee /opt/dockerfiles/nginx/index.html > /dev/null
+
 sudo docker run -d --name nginx \
+    -v /opt/dockerfiles/nginx:/usr/share/nginx/html
     -p 80:80 \
     nginx:1.27
 
