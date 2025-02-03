6fc795cc05bbbab9e276d321b2104d652611c1d7
Denis <denis.medel@holon.com.ar>
2025-02-03 16:55:45 -0300

    Curso DevOps Terraform + Github Actions

--- terraform/ec2-script.sh
+++ terraform/ec2-script.sh
@@ -1,22 +1,19 @@
 #! /bin/bash
 
-# Actualizar paquetes
 sudo apt update -y
 
-curl -fsSL https://get.docker.com | sh
+sudo curl -fsSL https://get.docker.com | sh
 
-sudo usermod -aG docker ${USER}
+sudo usermod -aG docker ubuntu
 
 sudo systemctl enable docker
 sudo systemctl start docker
 
-mkdir -p /opt/dockerfiles/nginx
-touch /opt/dockerfiles/nginx/index.html
-echo "Hola Curso" | sudo tee /opt/dockerfiles/nginx/index.html > /dev/null
-echo "1"
+sudo mkdir -p /opt/dockerfiles/nginx
+sudo touch /opt/dockerfiles/nginx/index.html
+sudo echo "Hola Curso" | sudo tee /opt/dockerfiles/nginx/index.html > /dev/null
 
 sudo docker run -d --name nginx \
     -v /opt/dockerfiles/nginx:/usr/share/nginx/html
     -p 80:80 \
     nginx:1.27
-
